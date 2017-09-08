9b3c7740b312c91f37fddf91c9c162ae1d9a1de0
Алексей <ottenok.belogo@gmail.com>
2017-09-08 21:02:46 +0300

    Added Enzume tests

--- test/index.js
+++ test/index.js
@@ -1,99 +1,145 @@
-import MyForm from "../src/helpers/MyForm";
+
+import React from "react";
 import _ from "lodash";
 import assert from "assert";
+import { expect } from "chai";
+import { mount, render, shallow } from "enzyme";
+
+import { Provider } from "react-redux";
+import Form from "../src/containers/Form";
+import store from "../src/store/configureStore";
+import MyForm from "../src/helpers/MyForm";
+
 
 describe("Установка значений и проверка валидации", () => {
-  describe("#validate()", () => {
-    describe("Проверка корректных значений ФИО", () => {
-      const fioAcceptedArray = ["Иванов Иван Иванович", " Иванов     Иван     Иванович ", "George Iscander Zulcarnain"];
-      const makeTest = fio => {
-        it(`The"${ fio }" is valid`, () => {
-          MyForm.setData({
-            fio: `${ fio }`,
-            email: "kuligon@ya.ru",
-            phone: "+7(111)222-33-11"
-          });
-          assert(_.isEqual(MyForm.validate(), { isValid: true, errorFields: [] }));
+  describe("Проверка корректных значений ФИО", () => {
+    const fioAcceptedArray = ["Иванов Иван Иванович", " Иванов     Иван     Иванович ", "George Iscander Zulcarnain"];
+    const makeTest = fio => {
+      it(`The"${ fio }" is valid`, () => {
+        MyForm.setData({
+          fio: `${ fio }`,
+          email: "kuligon@ya.ru",
+          phone: "+7(111)222-33-11"
         });
-      };
-      fioAcceptedArray.forEach(fioItem => makeTest(fioItem));
-    });
+        assert(_.isEqual(MyForm.validate(), { isValid: true, errorFields: [] }));
+      });
+    };
+    fioAcceptedArray.forEach(fioItem => makeTest(fioItem));
+  });
 
-    describe("Проверка некорректных значений ФИО", () => {
-      const fioNotAcceptedArray = ["Иван Иванов", "Ivan Ivanov 215", "Иван Иванов Иванович Ивановский", "123 123 123"];
-      const makeTest = fio => {
-        it(`The"${ fio }" is not valid`, () => {
-          MyForm.setData({
-            fio: `${ fio }`,
-            email: "kuligon@ya.ru",
-            phone: "+7(111)222-33-11"
-          });
-          assert(_.isEqual(MyForm.validate(), { isValid: false, errorFields: ["fio"] }));
+  describe("Проверка некорректных значений ФИО", () => {
+    const fioNotAcceptedArray = ["Иван Иванов", "Ivan Ivanov 215", "Иван Иванов Иванович Ивановский", "123 123 123"];
+    const makeTest = fio => {
+      it(`The"${ fio }" is not valid`, () => {
+        MyForm.setData({
+          fio: `${ fio }`,
+          email: "kuligon@ya.ru",
+          phone: "+7(111)222-33-11"
         });
-      };
-      fioNotAcceptedArray.forEach(fioItem => makeTest(fioItem));
-    });
+        assert(_.isEqual(MyForm.validate(), { isValid: false, errorFields: ["fio"] }));
+      });
+    };
+    fioNotAcceptedArray.forEach(fioItem => makeTest(fioItem));
+  });
 
-    describe("Проверка корректных значений почты", () => {
-      const emailAcceptedArray = ["ya.ru", "yandex.ru", "yandex.ua", "yandex.by", "yandex.kz", "yandex.com"];
-      const makeTest = email => {
-        it(`The"${ email }" is valid`, () => {
-          MyForm.setData({
-            fio: "Иванов Иван Иванович",
-            email: `kuligon@${ email }`,
-            phone: "+7(111)222-33-11"
-          });
-          assert(_.isEqual(MyForm.validate(), { isValid: true, errorFields: [] }));
+  describe("Проверка корректных значений почты", () => {
+    const emailAcceptedArray = ["ya.ru", "yandex.ru", "yandex.ua", "yandex.by", "yandex.kz", "yandex.com"];
+    const makeTest = email => {
+      it(`The"${ email }" is valid`, () => {
+        MyForm.setData({
+          fio: "Иванов Иван Иванович",
+          email: `kuligon@${ email }`,
+          phone: "+7(111)222-33-11"
         });
-      };
-      emailAcceptedArray.forEach(emailItem => makeTest(emailItem));
-    });
+        assert(_.isEqual(MyForm.validate(), { isValid: true, errorFields: [] }));
+      });
+    };
+    emailAcceptedArray.forEach(emailItem => makeTest(emailItem));
+  });
 
-    describe("Проверка некорректных значений почты", () => {
-      const emailNotAcceptedArray = ["ya.kz", "ya.ua", "mail.ru", "yandex.com.ru", "ya.ru.com", ""];
-      const makeTest = email => {
-        const errorFieldsArray = [];
-        it(`The "${ email }" is not valid`, () => {
-          MyForm.setData({
-            fio: "Иванов Иван Иванович",
-            email: `kuligon@${ email }`,
-            phone: "+7(111)222-33-11"
-          });
-          assert(_.isEqual(MyForm.validate(), { isValid: false, errorFields: ["email"] }));
+  describe("Проверка некорректных значений почты", () => {
+    const emailNotAcceptedArray = ["ya.kz", "ya.ua", "mail.ru", "yandex.com.ru", "ya.ru.com", ""];
+    const makeTest = email => {
+      const errorFieldsArray = [];
+      it(`The "${ email }" is not valid`, () => {
+        MyForm.setData({
+          fio: "Иванов Иван Иванович",
+          email: `kuligon@${ email }`,
+          phone: "+7(111)222-33-11"
         });
-      };
-      emailNotAcceptedArray.forEach(emailItem => makeTest(emailItem));
-    });
+        assert(_.isEqual(MyForm.validate(), { isValid: false, errorFields: ["email"] }));
+      });
+    };
+    emailNotAcceptedArray.forEach(emailItem => makeTest(emailItem));
+  });
 
-    describe("Проверка корректных значений телефона", () => {
-      const phoneAcceptedArray = ["+7(111)222-33-11", "+7(111)555-50-00", "+7(111)000-00-00"];
-      const makeTest = phone => {
-        it(`The"${ phone }" is valid`, () => {
-          MyForm.setData({
-            fio: "Иванов Иван Иванович",
-            email: "kuligon@ya.ru",
-            phone: `${phone}`
-          });
-          assert(_.isEqual(MyForm.validate(), { isValid: true, errorFields: [] }));
+  describe("Проверка корректных значений телефона", () => {
+    const phoneAcceptedArray = ["+7(111)222-33-11", "+7(111)555-50-00", "+7(111)000-00-00"];
+    const makeTest = phone => {
+      it(`The"${ phone }" is valid`, () => {
+        MyForm.setData({
+          fio: "Иванов Иван Иванович",
+          email: "kuligon@ya.ru",
+          phone: `${phone}`
         });
-      };
-      phoneAcceptedArray.forEach(phoneItem => makeTest(phoneItem));
-    });
+        assert(_.isEqual(MyForm.validate(), { isValid: true, errorFields: [] }));
+      });
+    };
+    phoneAcceptedArray.forEach(phoneItem => makeTest(phoneItem));
+  });
 
-    describe("Проверка некорректных значений телефона", () => {
-      const phoneNotAcceptedArray = ["+7 (111) 000-00-00", "+7(111)555-50-01", "8(111)000-00-00",
-        "+72224445566", "+7(999)999-99-99"];
-      const makeTest = phone => {
-        it(`The"${ phone }" is not valid`, () => {
-          MyForm.setData({
-            fio: "Иванов Иван Иванович",
-            email: "kuligon@ya.ru",
-            phone: `${phone}`
-          });
-          assert(_.isEqual(MyForm.validate(), { isValid: false, errorFields: ["phone"] }));
+  describe("Проверка некорректных значений телефона", () => {
+    const phoneNotAcceptedArray = ["+7 (111) 000-00-00", "+7(111)555-50-01", "8(111)000-00-00",
+      "+72224445566", "+7(999)999-99-99"];
+    const makeTest = phone => {
+      it(`The"${ phone }" is not valid`, () => {
+        MyForm.setData({
+          fio: "Иванов Иван Иванович",
+          email: "kuligon@ya.ru",
+          phone: `${phone}`
         });
-      };
-      phoneNotAcceptedArray.forEach(phoneItem => makeTest(phoneItem));
+        assert(_.isEqual(MyForm.validate(), { isValid: false, errorFields: ["phone"] }));
+      });
+    };
+    phoneNotAcceptedArray.forEach(phoneItem => makeTest(phoneItem));
+  });
+});
+
+describe("Тестирование <From />", () => {
+  window.MyForm = MyForm;
+  const wrapper = mount(
+    <Provider store={store}>
+      <Form />
+    </Provider>
+  );
+
+  describe("Тестирование input-ов", () => {
+    const inputs = wrapper.find("input");
+    it("На странице задана форма с тремя инпутами c соответствующими props", () => {
+      expect(inputs).to.have.length(3);
+
+      const firstInput = inputs.at(0);
+      expect(firstInput.props().name).to.equal("fio");
+
+      const secondInput = inputs.at(1);
+      expect(secondInput.props().name).to.equal("email");
+
+      const thirdInput = inputs.at(2);
+      expect(thirdInput.props().name).to.equal("phone");
     });
   });
+
+  describe("Тестирование кнопки отправки формы", () => {
+    it("На странице задана форма с кнопкой отправки формы", () => {
+      expect(wrapper.find("#submitButton").exists()).to.equal(true);
+    });
+  });
+
+  describe("Тестирование div-контейнера", () => {
+    it("На странице задан div-контейнер", () => {
+      expect(wrapper.find("#resultContainer").exists()).to.equal(true);
+    });
+  });
+
+
 });
